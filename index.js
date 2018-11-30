4e892489f5cbd0a39051f9cc3fcc0a1c36c021bc
wachirawit wacharak <tauhoo_ice@hotmail.com>
2018-11-30 23:54:26 +0700

    remove index.

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ var { bg, fg } = require('ervy/lib/utils')
 function isClusterEqual(firstClusters, secondClusters) {
   for (let [index, cluster1] of firstClusters.entries()) {
     cluster2 = secondClusters[index]
-    if (cluster1.index !== cluster2.index) return false
     if (cluster1.color !== cluster2.color) return false
   }
   return true
 }
-function distance(x1, y1, x2, y2) {
-  return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2))
+function distance(x1, x2) {
+  return Math.sqrt(Math.pow(x1 - x2, 2))
 }
 function Nearest([firstDistance, ...distanceFromClusters]) {
   oldDistance = firstDistance
@@ -27,8 +26,8 @@ function group(clusters, pixels) {
   groups = []
   pixels.forEach((pixelColor, pixelIndex) => {
     distanceFromClusters = []
-    clusters.forEach(({ color, index }) => {
-      distanceFromClusters.push(distance(color, index, pixelColor, pixelIndex))
+    clusters.forEach(({ color }) => {
+      distanceFromClusters.push(distance(color, pixelColor))
     })
     groups.push({
       color: pixelColor,
@@ -39,23 +38,21 @@ function group(clusters, pixels) {
   return groups
 }
 function updateClusters(clusters, groups) {
-  newClusters = clusters.map((value, index) => ({
+  newClusters = clusters.map(() => ({
     sumColor: 0,
     sumIndex: 0,
     count: 0,
   }))
-  groups.forEach(({ color, index, cluster }) => {
-    let { sumColor, sumIndex, count } = newClusters[cluster]
-    if (cluster === 3) console.log(sumColor, sumIndex, count)
+  groups.forEach(({ color, cluster }) => {
+    let { sumColor, count } = newClusters[cluster]
+    if (cluster === 3) console.log(sumColor, count)
     newClusters[cluster] = {
       sumColor: sumColor + color,
-      sumIndex: sumIndex + index,
       count: count + 1,
     }
   })
-  return newClusters.map(({ sumColor, sumIndex, count }) => ({
+  return newClusters.map(({ sumColor, count }) => ({
     color: count === 0 ? 0 : Math.floor(sumColor / count),
-    index: count === 0 ? 0 : Math.floor(sumIndex / count),
   }))
 }
 png.decode('./images/smalltest.png', pixels => {
@@ -63,34 +60,32 @@ png.decode('./images/smalltest.png', pixels => {
   clusters = [
     {
       color: 200,
-      index: 300,
     },
     {
       color: 100,
-      index: 200,
     },
     {
       color: 50,
-      index: 100,
     },
     {
       color: 250,
-      index: 50,
     },
   ]
   groups = group(clusters, pixels)
   newClusters = updateClusters(clusters, groups)
+  /*
   colors = ['red', 'green', 'blue', 'yellow']
   scatterData = groups.map(({ color, index, cluster }) => ({
     key: cluster + '',
     value: [index, color],
     style: fg(colors[cluster], '*'),
   }))
-  console.log(scatter(scatterData, { legendGap: 100, width: 50, height: 50 }))
-  /*
+  console.log(scatter(scatterData, { legendGap: 100, width: 50, height: 50 }))*/
+
   while (!isClusterEqual(clusters, newClusters)) {
-  groups = group(newClusters, pixels)
-  clusters = newClusters
-  newClusters = updateClusters(clusters, groups)
-  }*/
+    groups = group(newClusters, pixels)
+    clusters = newClusters
+    newClusters = updateClusters(clusters, groups)
+  }
+  console.log(newClusters)
 })
