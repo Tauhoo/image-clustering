d0795f809f0ec2fa46e2364defd68621b84d5e39
wachirawit wacharak <tauhoo_ice@hotmail.com>
2018-12-01 00:42:35 +0700

    cant make png but have some bug

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var png = require('png-js')
+var fs = require('fs')
+var PNG = require('pngjs').PNG
 var scatter = require('ervy/lib/scatter')
 var { bg, fg } = require('ervy/lib/utils')
 function isClusterEqual(firstClusters, secondClusters) {
@@ -55,6 +57,29 @@ function updateClusters(clusters, groups) {
     color: count === 0 ? 0 : Math.floor(sumColor / count),
   }))
 }
+
+function createPNG(groups, clusterSelect) {
+  var png = new PNG({
+    width: 10,
+    height: 10,
+  })
+  data = groups.map(({ color, index, cluster }) => [
+    color,
+    cluster == clusterSelect,
+  ])
+  for (var y = 0; y < png.height; y++) {
+    for (var x = 0; x < png.width; x++) {
+      var idx = (png.width * y + x) << 2
+      let select = data[idx][1]
+      png.data[idx] = select ? 255 : data[idx]
+      png.data[idx + 1] = select ? 218 : data[idx + 1]
+      png.data[idx + 2] = select ? 185 : data[idx + 2]
+      png.data[idx + 3] = select ? 128 : data[idx + 3]
+    }
+  }
+  png.pack().pipe(fs.createWriteStream('newOut.png'))
+}
+
 png.decode('./images/smalltest.png', pixels => {
   groups = []
   clusters = [
@@ -73,19 +98,11 @@ png.decode('./images/smalltest.png', pixels => {
   ]
   groups = group(clusters, pixels)
   newClusters = updateClusters(clusters, groups)
-  /*
-  colors = ['red', 'green', 'blue', 'yellow']
-  scatterData = groups.map(({ color, index, cluster }) => ({
-    key: cluster + '',
-    value: [index, color],
-    style: fg(colors[cluster], '*'),
-  }))
-  console.log(scatter(scatterData, { legendGap: 100, width: 50, height: 50 }))*/
-
   while (!isClusterEqual(clusters, newClusters)) {
     groups = group(newClusters, pixels)
     clusters = newClusters
     newClusters = updateClusters(clusters, groups)
   }
-  console.log(newClusters)
+  console.log(newClusters, groups.length)
+  createPNG(groups, 0)
 })
